15ae7e0a8370bf5194bf4cb6d0fa918340af0512
Nima Emamian <nimaem77@gmail.com>
2019-05-04 22:23:23 -0400

    Home Page links

--- CriterionAutomation/src/Chrome/HomePageImages.java
+++ CriterionAutomation/src/Chrome/HomePageImages.java
@@ -1,10 +1,14 @@
 package Chrome;
+
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -14,35 +18,60 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+/**
+ * @author homefolder
+ *
+ */
 public class HomePageImages {
- 	private static ChromeDriver driver;
-	  private String baseUrl;
-	  private StringBuffer verificationErrors = new StringBuffer();
+	private static ChromeDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-	  @Before
-	  public void setUp() throws Exception {
-			 System.setProperty("webdriver.chrome.driver", "/Users/homefolder/Desktop/JavaCode/chromedriver");
+	@BeforeClass
+	public static void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/homefolder/Desktop/JavaCode/chromedriver");
 
-	    driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
-	    baseUrl = "https://www.criterion.com";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
+	}
+	
+	
 
-	  @Test
-	  public void testCollectionHomePageScroll() throws Exception {
-	    driver.get("https://www.criterion.com");
-	    driver.findElement(By.xpath("//*[contains(text(), 'New Release')]")).click();
-	    
-	  }
+	@Test
+	public void testA() throws Exception {
 
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.close();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
+		driver.get("https://www.criterion.com");
+		Thread.sleep(300);
+		driver.findElement(
+				By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='My Criterion'])[2]/following::img[1]"))
+				.click();
+		Thread.sleep(1000);
+		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(1));
+		  String url =  driver.getCurrentUrl();
+			Thread.sleep(1000);
+		    driver.switchTo().window(tabs.get(0));	  
+
+		  assertTrue(url.equals("http://www.janusfilms.com/"));
+
+	}
+
+	@Test
+	public void testB() throws Exception {
+		driver.findElement(
+				By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='My Criterion'])[2]/following::img[2]"))
+				.click();
+		Thread.sleep(1000);
+		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(2));
+		  String url =  driver.getCurrentUrl();
+		assertTrue(url.equals("https://www.criterionchannel.com/"));
+
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
 
 }
---
+++ CriterionAutomation/src/Chrome/HomeSocialMediaLinks.java
@@ -0,0 +1,97 @@
+package Chrome;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class HomeSocialMediaLinks {
+	private static ChromeDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/homefolder/Desktop/JavaCode/chromedriver");
+
+		driver = new ChromeDriver();
+
+	}
+	
+	
+	//facebook
+	@Test
+	public void testA() throws Exception {
+
+		driver.get("https://www.criterion.com");
+		driver.findElement(By.cssSelector("li > a > svg > path")).click();
+		Thread.sleep(8600);
+		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(1));
+		  String url =  driver.getCurrentUrl();
+			Thread.sleep(800);
+		    driver.switchTo().window(tabs.get(0));	  
+
+		  assertTrue(url.equals("https://www.facebook.com/CriterionCollection"));
+
+	}
+
+	
+	@Test
+	public void testB() throws Exception {
+		driver.findElement(By.xpath("//a[@href='https://twitter.com/Criterion']")).click();
+		Thread.sleep(1000);
+		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(2));
+		  String url =  driver.getCurrentUrl();
+			Thread.sleep(1000);
+
+		    driver.switchTo().window(tabs.get(0));	  
+
+		assertTrue(url.equals("https://twitter.com/Criterion"));
+
+	}
+	@Test
+	public void testC() throws Exception {
+		driver.findElement(By.xpath("//a[@href='http://criterioncollection.tumblr.com/']")).click();
+		Thread.sleep(1000);
+		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(3));
+		  String url =  driver.getCurrentUrl();
+			Thread.sleep(1000);
+
+		    driver.switchTo().window(tabs.get(0));	  
+
+		assertTrue(url.equals("https://criterioncollection.tumblr.com/"));
+
+	}
+	@Test
+	public void testD() throws Exception {
+		Thread.sleep(600);
+		driver.findElement(By.xpath("//a[@href='https://www.youtube.com/user/criterioncollection']")).click();
+		Thread.sleep(1000);
+		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(4));
+		  String url =  driver.getCurrentUrl();
+			Thread.sleep(1000);
+
+		    driver.switchTo().window(tabs.get(0));	  
+		assertTrue(url.equals("https://www.youtube.com/user/criterioncollection"));
+
+	}
+	
+	
+	@AfterClass
+	public static void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
+}
